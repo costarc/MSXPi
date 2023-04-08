d3cb1736d4645fc0bf30be6b6017b1be26bb90af
Roni <ronivon.costa@gmail.com>
2023-04-08 17:46:01 +0100

    MSXPIUPD.bat update

--- software/target/msxpiupd.bat
+++ software/target/msxpiupd.bat
@@ -1,44 +1,26 @@
 echo Updating MSXPi client and server software...
 pcd /home/pi/msxpi
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/API.BAS?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/IRC.BAS?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/IRCv2.BAS?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/PRECONN.BAS?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/at28c256.com?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/dosinit.com?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/msxpidos.rom?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/msxpiext.bin?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pcd.com?raw=true
-echo
-pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pcopy.com?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pdate.com?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pdir.com?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pplay.com?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/prun.com?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pset.com?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pver.com?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pvol.com?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pwifi.com?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/templat2.com?raw=true
-echo
 pcopy https://github.com/costarc/MSXPi/raw/master/software/target/template.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pcopy.com?raw=true pcopynew.com
+COPY PCOPYNEW.COM PCOPY.COM
 echo
 echo Updating msxpi-server.py...
 prun wget -q --show-progress -O msxpi-server.py https://tinyurl.com/msxpi-server
