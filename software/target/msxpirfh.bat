5f01345811b9300011dae445126d0ddec5b8c9d4
Roni <ronivon.costa@gmail.com>
2023-09-11 19:58:31 +0100

    Improved MSXPIUPD process

---
+++ software/target/msxpirfh.bat
@@ -0,0 +1,41 @@
+pcd /home/pi/msxpi
+ 
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/at28c256.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/chatgpt.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/dosinit.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/msxpidos.rom?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/msxpiext.bin?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/multirom.rom?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pcd.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pdate.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pdatedeb.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pdir.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pplay.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/preboot.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/prestart.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/prun.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pset.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pshut.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pver.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pvol.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pwifi.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/python.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/template.com?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/BASIC/API.BAS?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/BASIC/DOLAR.BAS?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/BASIC/IRC.BAS?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/BASIC/WEATHER.BAS?raw=true
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/pcopy.com?raw=true pcopynew.com
+COPY PCOPYNEW.COM PCOPY.COM
+DEL PCOPYNEW.COM
+echo
+echo Updating msxpi-server.py ...
+prun wget -q --show-progress -O msxpi-server.py https://tinyurl.com/msxpi-server
+echo
+echo Updating MSXPi boot disk ...
+prun wget -q --show-progress -O disks/msxpiboot.dsk https://tinyurl.com/msxpibootdisk
+echo
+echo Updating MSXPi tools disk ...
+prun wget -q --show-progress -O disks/tools.dsk https://tinyurl.com/toolsdisk
+prestart
+echo
