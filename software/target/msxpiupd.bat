2a9712f6a37b3b48c2069b6ad247e37fd03c81aa
Roni <ronivon.costa@gmail.com>
2023-09-15 11:02:18 +0100

    Now using drive M: for MSXPi updates

--- software/target/msxpiupd.bat
+++ software/target/msxpiupd.bat
@@ -1,8 +1,8 @@
 echo Preparing to update...
+pcopy https://github.com/costarc/MSXPi/raw/master/software/target/msxpiupd.bat
 pset DriveM https://github.com/costarc/MSXPi/raw/master/software/target
 pdate
 echo Getting lastest updater...
-pcopy m:msxpiupd.bat
 pcopy m:msxpirfh.bat
 echo 
 echo Starting update
